f4f455699aa4a223959ebb031ec270864ee479bd
SorahISA <46983782+SorahISA@users.noreply.github.com>
2020-05-24 16:41:45 +0800

    記事本 (Notepad) | O(|S|) stack

---
+++ APCS/202004 APCS 模擬測驗/C.cpp
@@ -0,0 +1,60 @@
+/* https://hackmd.io/@joylintp/APCSM_202004 */
+
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(1, 1E8); \
+                 auto rnd = bind(__dis, __gen)
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+
+int32_t main() {
+    fastIO();
+    
+    string s;
+    cin >> s;
+    
+    stack<char> left, right;
+    for (auto c : s) {
+        if (islower(c)) left.push(c);
+        if (c == 'L' and  !left.empty()) right.push( left.top()),  left.pop();
+        if (c == 'R' and !right.empty())  left.push(right.top()), right.pop();
+        if (c == 'B' and  !left.empty())  left.pop();
+        if (c == 'D' and !right.empty()) right.pop();
+    }
+    
+    string ans;
+    while (!left.empty()) ans.pb(left.top()), left.pop();
+    reverse(ALL(ans));
+    while (!right.empty()) ans.pb(right.top()), right.pop();
+    
+    cout << ans << "\n";
+    
+    return 0;
+}
