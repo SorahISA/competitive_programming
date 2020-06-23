38a524bd72269339b8071bd5bfd1c947d8690aaa
SorahISA <46983782+SorahISA@users.noreply.github.com>
2020-06-23 16:09:22 +0800

    Johnny and Contribution | O(n + m) counting sort + pointer

---
+++ Codeforces/1361 - Codeforces Round #647 (Div. 1)/1361A.cpp
@@ -0,0 +1,68 @@
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(1, 1E8); \
+                 auto rnd = bind(__dis, __gen)
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+
+int32_t main() {
+    fastIO();
+    
+    int n, m, u, v, fl = 1;
+    cin >> n >> m;
+    
+    vector<int> adj[n], col(n), ncol(n, 1); /// color, now color
+    while (m--) cin >> u >> v, --u, --v, adj[u].eb(v), adj[v].eb(u);
+    for (auto &x : col) cin >> x;
+    
+    vector<int> cnt_sort[n], seq;
+    for (int i = 0; i < n; ++i) cnt_sort[col[i] - 1].eb(i);
+    for (int i = 0; i < n; ++i) {
+        for (auto x : cnt_sort[i]) seq.eb(x);
+    }
+    
+    for (auto now : seq) {
+        for (auto x : adj[now]) {
+            if (ncol[x] == col[now]) ++ncol[x];
+        }
+    }
+    
+    for (int i = 0; i < n; ++i) fl &= ncol[i] == col[i];
+    
+    if (fl) {
+        for (auto x : seq) cout << x+1 << " ";
+        cout << "\n";
+    }
+    else {
+        cout << -1 << "\n";
+    }
+    
+    return 0;
+}
