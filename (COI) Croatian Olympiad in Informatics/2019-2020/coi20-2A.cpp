e586d2c3857844b49c292c209dd9ba41fa24d14a
SorahISA <46983782+SorahISA@users.noreply.github.com>
2020-06-24 21:06:03 +0800

    ACM

---
+++ (COI) Croatian Olympiad in Informatics/2019-2020/coi20-2A.cpp
@@ -0,0 +1,84 @@
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(1, 1E8); \
+                 auto rnd = bind(__dis, __gen)
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+
+int32_t main() {
+    fastIO();
+    
+    int n, m, prob, pen;
+    cin >> n >> m;
+    
+    string name, score;
+    vector<pair<string, pii>> penalty;
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> name, prob = pen = 0;
+        for (int j = 0; j < m; ++j) {
+            cin >> score;
+            if (score[0] == '?' or score[0] == '+') {
+                ++prob;
+                pen += ((score[1]^'0') - 1) * 20 * 60;
+                pen += stoi(score.substr(3, 2)) * 60 * 60;
+                pen += stoi(score.substr(6, 2)) * 60;
+                pen += stoi(score.substr(9, 2));
+            }
+        }
+        
+        if (name != "NijeZivotJedanACM") penalty.pb({name, {prob, pen}});
+    }
+    
+    cin >> name, prob = pen = 0;
+    for (int i = 0; i < m; ++i) {
+        cin >> score;
+        if (score[0] == '?' or score[0] == '+') {
+            ++prob;
+            pen += ((score[1]^'0') - 1) * 20 * 60;
+            pen += stoi(score.substr(3, 2)) * 60 * 60;
+            pen += stoi(score.substr(6, 2)) * 60;
+            pen += stoi(score.substr(9, 2));
+        }
+    }
+    penalty.pb({name, {prob, pen}});
+    
+    sort(ALL(penalty), [](auto a, auto b) {
+        if (a.Y == b.Y) return a.X < b.X;
+        if (a.Y.X == b.Y.X) return a.Y.Y < b.Y.Y;
+        return a.Y.X > b.Y.X;
+    });
+    
+    for (int i = 0; i < n; ++i) {
+        if (penalty[i].X == name) return cout << i+1 << "\n", 0;
+    }
+    
+    return 0;
+}
