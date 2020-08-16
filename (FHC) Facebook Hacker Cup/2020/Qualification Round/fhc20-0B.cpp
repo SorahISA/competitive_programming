8ad69d806875e1f248fe05138cc1c6024a589888
SorahISA <46983782+SorahISA@users.noreply.github.com>
2020-08-16 23:09:33 +0800

    Alchemy | O(N)

---
+++ (FHC) Facebook Hacker Cup/2020/Qualification Round/fhc20-0B.cpp
@@ -0,0 +1,60 @@
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(1, 1E8); \
+                 auto rnd = bind(__dis, __gen)
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+
+void solve() {
+    int n, cntA = 0;
+    string s;
+    cin >> n >> s;
+    
+    for (auto c : s) cntA += c == 'A';
+    if (cntA == n / 2 or cntA == n / 2 + 1) cout << "Y\n";
+    else cout << "N\n";
+}
+
+int32_t main() {
+    fastIO();
+#ifndef local
+    freopen("testI.txt", "r", stdin);
+    freopen("testO.txt", "w", stdout);
+#endif
+    
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; ++tc) {
+        cout << "Case #" << tc << ": ";
+        solve();
+    }
+    
+    return 0;
+}
