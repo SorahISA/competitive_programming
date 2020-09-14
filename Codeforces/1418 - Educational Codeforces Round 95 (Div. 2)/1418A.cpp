d97aacb3d3ab172e71f6e6ec61191663a8646ad2
SorahISA <46983782+SorahISA@users.noreply.github.com>
2020-09-15 00:49:47 +0800

    Buying Torches

---
+++ Codeforces/1418 - Educational Codeforces Round 95 (Div. 2)/1418A.cpp
@@ -0,0 +1,52 @@
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(1, 1E8); \
+                 auto rnd = bind(__dis, __gen)
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+const int maxn = 2E5 + 5;
+
+void solve() {
+    int x, y, k;
+    cin >> x >> y >> k;
+    
+    int need = y * k + k;
+    cout << (need - 2) / (x - 1) + 1 + k << "\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
