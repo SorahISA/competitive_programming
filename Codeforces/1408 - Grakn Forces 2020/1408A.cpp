313318017762608f0c46e7c0a76f8fcf8af45e2d
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-01-04 00:58:53 +0800

    Circle Coloring

---
+++ Codeforces/1408 - Grakn Forces 2020/1408A.cpp
@@ -0,0 +1,61 @@
+// #pragma GCC target("avx2")
+#pragma GCC optimize("Ofast", "unroll-loops")
+
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+// template <typename T>
+// using pbds_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using pii = pair<int, int>;
+template<typename T>
+using prior = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using Prior = priority_queue<T>;
+
+#define X first
+#define Y second
+#define ALL(x) (x).begin(), (x).end()
+#define eb emplace_back
+#define pb push_back
+
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define RANDOM() random_device __rd; \
+                 mt19937 __gen = mt19937(__rd()); \
+                 uniform_int_distribution<int> __dis(0, 1); \
+                 auto rnd = bind(__dis, __gen);
+
+const int INF = 1E18;
+const int mod = 1E9 + 7;
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n), b(n), c(n);
+    for (auto &x : a) cin >> x;
+    for (auto &x : b) cin >> x;
+    for (auto &x : c) cin >> x;
+    
+    for (int i = 0; i < n-1; ++i) {
+        cout << a[i] << " ";
+        if (a[i] == a[i+1]) swap(a[i+1], b[i+1]);
+    }
+         if (a[n-1] != a[0] and a[n-1] != a[n-2]) cout << a[n-1] << "\n";
+    else if (b[n-1] != a[0] and b[n-1] != a[n-2]) cout << b[n-1] << "\n";
+    else if (c[n-1] != a[0] and c[n-1] != a[n-2]) cout << c[n-1] << "\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
