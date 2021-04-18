d8f517382efb3ed80383dd006937ddf1ee8fcbce
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-04-18 22:57:49 +0800

    God Sequence | O(A+B)

---
+++ AtCoder/AtCoder Beginner + Regular Contest/arc117/arc117_a.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+template<typename T>
+ostream& operator << (ostream &os, vector<T> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i) os << " ";
+        os << vec[i];
+    }
+    return os;
+}
+
+void solve() {
+    int A, B; cin >> A >> B;
+    
+    for (int i = 1; i <= A-1; ++i) cout << i << " ";
+    cout << (int)1E9 - A*(A-1)/2 << " ";
+    for (int i = 1; i <= B-1; ++i) cout << -i << " ";
+    cout << -((int)1E9 - B*(B-1)/2) << " ";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; // cin >> t;
+    for (int _ = 1; _ <= t; ++_) {
+        // cout << "Case #" << _ << ": ";
+        solve();
+    }
+    
+    return 0;
+}
