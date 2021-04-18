cf421fc916d96fff5ab23fdd6e239ce26437f15c
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-04-18 22:59:19 +0800

    ARC Wrecker | O(N lg N) sort + math

---
+++ AtCoder/AtCoder Beginner + Regular Contest/arc117/arc117_b.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+template<typename T>
+ostream& operator << (ostream &os, vector<T> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i) os << " ";
+        os << vec[i];
+    }
+    return os;
+}
+
+const int mod = 1E9 + 7;
+
+void solve() {
+    int n; cin >> n;
+    
+    vector<int> vec(n);
+    for (auto &x : vec) cin >> x;
+    sort(ALL(vec));
+    vec.resize(unique(ALL(vec)) - begin(vec));
+    
+    int ans = vec[0] + 1;
+    for (int i = 1; i < vec.size(); ++i) ans = ans * (vec[i] - vec[i-1] + 1) % mod;
+    cout << ans << "\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; // cin >> t;
+    for (int _ = 1; _ <= t; ++_) {
+        // cout << "Case #" << _ << ": ";
+        solve();
+    }
+    
+    return 0;
+}
