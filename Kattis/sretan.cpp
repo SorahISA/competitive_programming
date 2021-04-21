24a6c4fcb584e0e1777c76c095d92d120b824a03
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-04-21 23:16:05 +0800

    Sretan | O(lg K)

---
+++ Kattis/sretan.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+inline int getRand(int L, int R) {
+    if (L > R) swap(L, R);
+    return (int)(rng() % (uint64_t)(R - L + 1) + L);
+}
+
+template<typename T>
+ostream& operator << (ostream &os, vector<T> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i) os << " ";
+        os << vec[i];
+    }
+    return os;
+}
+
+void solve() {
+    int n, len; cin >> n, len = __lg(n+1);
+    ++n, n -= 1 << len;
+    
+    for (int i = len-1; i >= 0; --i) cout << "47"[n>>i&1];
+    cout << "\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; // cin >> t;
+    for (int _ = 1; _ <= t; ++_) {
+        // cout << "Case #" << _ << ": ";
+        solve();
+    }
+    
+    return 0;
+}
