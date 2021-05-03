18a5cbafff439909ad5b2394c6798657a00fe5dd
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-05-03 09:12:51 +0800

    Phoenix and Gold | O(n^1.5) greedy

---
+++ Codeforces/1515 - Codeforces Global Round 14/1515A.cpp
@@ -0,0 +1,68 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+inline int getRand(int L, int R) {
+    if (L > R) swap(L, R);
+    return (int)(rng() % (uint64_t)(R - L + 1) + L);
+}
+
+template<typename T1, typename T2>
+ostream& operator << (ostream &os, pair<T1, T2> p) {
+    os << "(" << p.first << "," << p.second << ")";
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream &os, vector<T> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i) os << " ";
+        os << vec[i];
+    }
+    return os;
+}
+
+void solve() {
+    int n, x; cin >> n >> x;
+    
+    vector<int> w(n);
+    for (auto &_ : w) cin >> _;
+    
+    if (accumulate(ALL(w), 0) == x) return cout << "NO\n", void();
+    
+    cout << "YES\n";
+    for (int i = 0, sum = 0; i < n; ++i) {
+        if (sum + w[i] == x) reverse(i + ALL(w));
+        sum += w[i];
+        cout << w[i] << " \n"[i == n-1];
+    }
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; cin >> t;
+    for (int _ = 1; _ <= t; ++_) {
+        // cout << "Case #" << _ << ": ";
+        solve();
+    }
+    
+    return 0;
+}
