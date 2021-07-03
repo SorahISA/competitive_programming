ad6ed1eb4dc7c8d059009991c4dc60a92e7ddb3f
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-07-03 23:15:59 +0800

    I Have A Dream

---
+++ TOKI/TSOC April Fools 2021/tsoc-april-fools-2021_I.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+void solve() {
+    string s; cin >> s;
+    for (auto x : s) {
+        if (x == 'i' or x == 'I') return cout << "YES\n", void();
+    }
+    cout << "NO\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
