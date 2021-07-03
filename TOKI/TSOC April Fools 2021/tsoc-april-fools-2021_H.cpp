1017b77c0a9c25b93fb8a1b740673e5556c5804b
SorahISA <46983782+SorahISA@users.noreply.github.com>
2021-07-03 23:15:47 +0800

    Huge Sudoku

---
+++ TOKI/TSOC April Fools 2021/tsoc-april-fools-2021_H.cpp
@@ -0,0 +1,32 @@
+#pragma GCC optimize("Ofast", "unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define double long double
+using pii = pair<int, int>;
+template<typename T>
+using Prior = std::priority_queue<T>;
+template<typename T>
+using prior = std::priority_queue<T, vector<T>, greater<T>>;
+
+#define X first
+#define Y second
+#define eb emplace_back
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0)
+
+void solve() {
+    int n; cin >> n;
+    cout << n*n * (n*n+1) / 2 << "\n";
+}
+
+int32_t main() {
+    fastIO();
+    
+    int t = 1; // cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
